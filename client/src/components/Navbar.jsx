5372d41d22070823e3b56830324d99913151464f
pawida chumpurat <pawida.may@gmail.com>
2023-04-05 23:07:57 +0700

    feat(homepage): update navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import { Text, Button} from "@chakra-ui/react";
+import { Text, Button } from "@chakra-ui/react";
 
 function Navbar() {
+
+
   return (
-    <div className="flex flex-row bg-blue-300  px-[10%]  py-[2%] justify-between w-[1440px] h-[88px] items-center">
-      <Text className="text-headline2 fontWeight-headline2" shadow='shadow1'>CourseFlow</Text>
+      
+    <div className="flex flex-row  px-[10%]  py-[2%] justify-between w-[1440px] h-[88px] items-center shadow-md">
+      <Text className="text-body1 font-bold" 
+      bgGradient='linear-gradient(90deg, #95BEFF, #0040E5)'
+      bgClip='text'
+      >
+        CourseFlow
+        </Text>
       <div className="flex justify-between w-[300px] items-center">
-        <h1 className="text-[#191C77] font-bold">Our Courses</h1>
-        <Button variant='primary'> Log in</Button>
+        <div className="text-[#191C77] font-bold">Our Courses</div>
+        <Button variant='primary'>Log in</Button>
       </div>
     </div>
+    
   );
 }
 
