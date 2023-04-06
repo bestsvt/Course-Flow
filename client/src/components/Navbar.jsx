86ace378bead1f991be0ee3fa00ce3b32f77486e
S_Best <bestbest116@gmail.com>
2023-04-06 10:55:47 +0700

    fix(homepage): fix conflict and refactor code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Text, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 function Navbar() {
 
 
   return (
       
-    <div className="flex flex-row  px-[10%]  py-[2%] justify-between h-[88px] items-center shadow-md">
-      <img src="./image/navbar/CourseFlow.png"/>
+    <div className="flex flex-row px-[10%] justify-between h-[88px] items-center shadow-shadow2 z-5 relative">
+      <img src="./image/logo/CourseFlow.png" className="w-[200px]"/>
       <div className="flex justify-between w-[300px] items-center">
         <div className="text-[#191C77] font-bold">Our Courses</div>
         <Button variant='primary'>Log in</Button>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import Navbar from "../components/Navbar";
 import SubFooter from "../components/SubFooter"
 import Footer from "../components/Footer"
 import {instructor} from "../mockdata/instructor.js"
+import { Button } from '@chakra-ui/react'
+import Vector1 from "/image/header/Vector1.png"
 
 function HomePage() {
     
@@ -10,9 +12,24 @@ function HomePage() {
         <Navbar/>
         {/* ———————— Section Header ———————— */}
         <header>
-            {/* Start Coding Here */}
-
-        </header>
+        <div className="flex flex-row justify-between px-[10%] py-[7.5%] bg-blue-100 bg-imag-header">
+            <div className="flex flex-col gap-6">
+                <h1 className="text-headline1 text-black font-headline1">
+                Best Virtual <br/>
+                Classroom Software
+                </h1>
+                <h2 className="text-body1 text-gray.700 font-body1">
+                Welcome to Schooler! The one-stop online class management<br/>
+                system that caters to all your educational needs!
+                </h2>
+                <Button className="w-[200px] mt-[36px]" variant="primary">
+                Explore Courses
+                </Button>
+            </div>
+            <img className="w-[30%] mr-20"  
+            src={Vector1} alt="computer"/>
+        </div>
+      </header>
         {/* ———————— Section 2 ———————— */}
         <section>
             {/* Start Coding Here */}
@@ -20,20 +37,19 @@ function HomePage() {
         </section>
 
         {/* ———————— Section 3 ———————— */}
-        <section className="px-[10%] my-[105px]">
-            {/* Start Coding Here */}
-            <div className="text-center text-headline2 text-black font-headline2 mb-10" >Our Professional Instructor</div>
-            <div className="flex justify-center">
-        {instructor.map((data,index)=>{
-            return (
-                <div key={index} className="flex-col mx-6 text-center">
-                    <img src={data.image} alt={data.altDescription} className="w-[336px] h-[395px]"/>
-                    <div className="text-headline3 font-headline3 text-black mt-4">{data.name}</div>
-                    <div className="text-blue-400">{data.position}</div>
-                </div> 
-            )
-        })}
-        </div>
+        <section className="px-[10%] py-[5%] flex flex-col gap-16">
+            <div className="text-center text-headline2 text-black font-headline2">Our Professional Instructor</div>
+            <div className="flex justify-between">
+            {instructor.map((data,index)=>{
+                return (
+                    <div key={index} className="flex-col text-center w-[30%]">
+                        <img src={data.image} alt={data.altDescription} className="w-full"/>
+                        <div className="text-headline3 font-headline3 text-black mt-4">{data.name}</div>
+                        <div className="text-blue-400">{data.position}</div>
+                    </div> 
+                )
+            })}
+            </div>
         </section>
 
 
