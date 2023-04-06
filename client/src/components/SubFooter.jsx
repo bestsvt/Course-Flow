64e69f4e3dc29a2bbe5cd7183d502c033f259fa8
Kingkarn <kingkarn.saipadit@gmail.com>
2023-04-06 11:26:40 +0700

    feat(footer): add code and style footer

--- client/src/components/SubFooter.jsx
+++ client/src/components/SubFooter.jsx
@@ -1,7 +1,17 @@
+import { Button } from '@chakra-ui/react'
+
 function SubFooter() {
     
     return (
-        <div>this is SubFooter section</div>
+        <div className="flex flex-row justify-around items-center bg-blue-500 h-[500px]"  >
+            <div className="box-left flex flex-col justify-around items-start">
+                <h1 className="text-headline2 text-white">Interested in Becomming<br/>a Software Developer?</h1>
+                <Button variant="secondary" className="w-[250px] mt-10">Check Out Our Course</Button>
+            </div>
+            <div>
+            <img src=".\image\footer\Group.png" alt="Group"/>
+            </div>
+        </div>
     )
 }
 
