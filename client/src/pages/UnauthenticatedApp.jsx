11d3eb31b9fcc5006441f18e5ff32dce7cf03259
Baimorn <baimoen55@gmail.com>
2023-04-12 14:48:51 +0700

    feat(course-detail-page): add course detail page

--- client/src/pages/UnauthenticatedApp.jsx
+++ client/src/pages/UnauthenticatedApp.jsx
@@ -4,6 +4,7 @@ import HomePage from "./HomePage";
 import LoginPage from "./LogingPage"
 import NotFoundPage from "./NotFoundPage";
 import OurCoursePage from "./OurCoursePage";
+import CourseDetailPage from "./CourseDetailPage";
 
 function UnauthenticatedApp() {
   return (
@@ -13,6 +14,7 @@ function UnauthenticatedApp() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/courses" element={<OurCoursePage />} />
+        <Route path="/courses/:courseId" element={<CourseDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
