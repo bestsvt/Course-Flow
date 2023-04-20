82bc68cb2d9aaf3dae7badcbc9ebfeb0f0467b3d
Baimorn <baimoen55@gmail.com>
2023-04-20 10:20:06 +0700

    feat(learning): improve video function

--- client/src/pages/CourseLearningPage.jsx
+++ client/src/pages/CourseLearningPage.jsx
@@ -1,42 +1,46 @@
-import React, { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../contexts/authentication";
-import { Button, 
-Link, 
-Progress, 
-Accordion,
-AccordionItem,
-AccordionButton,
-AccordionPanel,
-AccordionIcon,
-Box,
-Badge,
-Spinner } from "@chakra-ui/react";
+import {
+    Button,
+    Link,
+    Progress,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box,
+    Badge,
+    Spinner
+} from "@chakra-ui/react";
 import useCourses from "../hooks/useCourses";
+import { update } from "lodash";
+
 
 function CourseLearningPage() {
-    const { isAuthenticated , userAuthState} = useAuth();
-    const { course , isLoading , getCoursesById , getSubLessonById} = useCourses();
+    const { isAuthenticated, userAuthState } = useAuth();
+    const { course, isLoading, getCoursesById, getSubLessonById } = useCourses();
     const navigate = useNavigate();
     const params = useParams()
-    const [lessonName , setLessonName ] = useState()
-    const [lessonVideo , setLessonVideo ] = useState()
-    const [statusVideo , setStatusVideo ] = useState()
-    
+    const [lesson, setLesson] = useState()
+    const [statusVideo, setStatusVideo] = useState()
+
     // Function get sub lesson to show name and video
     async function getSubLesson() {
         const result = await getSubLessonById()
-        setLessonName(result.name);
-        setLessonVideo(result.video);
+        setLesson(result);
+        console.log(result);
+
     }
 
     useEffect(() => {
         async function getCourses() {
             await getCoursesById(userAuthState.user.id);
             await getSubLesson()
-        }   
+        }
         getCourses()
     }, [navigate]);
 
@@ -48,6 +52,17 @@ function CourseLearningPage() {
         // Function axios.post
         // Upload status inProgress ใส่ใน table users_sub_lessons
         // ต้องเช็คด้วยว่า ถ้าหาก status เป็น complete อยู่แล้วจะไม่ update ข้อมูลเข้าไป
+        try {
+            const result = await axios.post(`http://localhost:4000/${course.course_id}/learning/${lesson.sub_lesson_id}?user=${userAuthState.user.id}`, {
+                status: statusVideo,
+                current_time: event.target.currentTime,
+                action: "play"
+            });
+            return result
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
     // This function work when click (Pause Video)
@@ -55,10 +70,21 @@ function CourseLearningPage() {
         // กรณีที่ video มันจบแล้วมันจะนับว่าเป็นการ pause ด้วย
         if (event.target.currentTime !== event.target.duration) {
             console.log("Pause at:", event.target.currentTime)
+            try {
+                const result = await axios.post(`http://localhost:4000/${course.course_id}/learning/${lesson.sub_lesson_id}?user=${userAuthState.user.id}`, {
+                    current_time: event.target.currentTime,
+                    updated_at: new Date(),
+                    action: "pause"
+                });
+                return result
+            } catch (error) {
+                console.log(error);
+            }
         }
         // Start Coding Here 2
         // Function axios.put (post หรือ put ?) (หลังบ้านอาจจะใช้ upsert)
         // Update currentTime ใส่ใน table users_sub_lessons
+
     }
 
     // This function work when video end
@@ -68,6 +94,17 @@ function CourseLearningPage() {
         // Start Coding Here 3
         // Function axios.put (post หรือ put ?) (หลังบ้านอาจจะใช้ upsert)
         // Update status complete ใส่ใน table users_sub_lessons
+        try {
+            const result = await axios.post(`http://localhost:4000/${course.course_id}/learning/${lesson.sub_lesson_id}?user=${userAuthState.user.id}`, {
+                status: statusVideo,
+                current_time: event.target.currentTime,
+                updated_at: new Date(),
+                action: "end"
+            });
+            return result
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // This function work when start video
@@ -88,7 +125,7 @@ function CourseLearningPage() {
                 {/* ———————— Left Section ———————— */}
                 <div className="shadow-shadow1 w-[30%] flex flex-col gap-6 px-6 py-8 overflow-y-scroll hide-scroll h-[1080px] rounded-lg">
                     <div className="text-orange-500 font-body3 mb-4">Course</div>
-                        {/* Course Deatil */}
+                    {/* Course Deatil */}
                     <div className="flex flex-col gap-2">
                         <div className="text-headline3 font-headline3 text-black">{course?.name}</div>
                         <div className="text-body2 font-body2 text-gray-700">{course?.course_summary}</div>
@@ -99,59 +136,59 @@ function CourseLearningPage() {
                         <div className="text-body3 font-body3 text-gray-700">5% Complete</div>
                         <Progress value={5} />
                     </div>
-                    
+
                     <Accordion allowMultiple>
-                        {course?.lessons.map((lesson,index)=>{
-                        return (
-                            <AccordionItem borderTop="none" key={index}>
-                                <AccordionButton paddingBottom={0}>
-                                    <Box as="span" flex='1' textAlign='left' padding='16px 0'>
-                                        <div className="flex gap-4">
-                                            <h1 className="text-headline3 font-headline3 text-gray-700" >0{index+1}</h1>
-                                            <h1 className="text-headline3 font-headline3 text-black">{lesson.name}</h1>
-                                        </div>
-                                    </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                                <AccordionPanel paddingTop='8px'>
-                                    <ul className="text-body2 font-body2 text-gray-700">
-                                    {lesson.sub_lessons.sort((a, b) => a.sub_lesson_id - b.sub_lesson_id).map((sub_lesson,index)=>{
-                                        return (
-                                        <li className="flex items-center px-2 py-3 gap-4 hover:cursor-pointer" key={index}
-                                        onClick={()=>{navigate(`/courses/1/learning/${sub_lesson.sub_lesson_id}`)}}
-                                        >
-                                        <img src="/image/icon/no-watch.png" alt="icon-status" className="w-[18px] h-[18px]"/> 
-                                        <p>{sub_lesson.name}</p>
-                                        </li>
-                                        )
-                                    })}
-                                    </ul>
-                                </AccordionPanel>
-                            </AccordionItem>
+                        {course?.lessons.map((lesson, index) => {
+                            return (
+                                <AccordionItem borderTop="none" key={index}>
+                                    <AccordionButton paddingBottom={0}>
+                                        <Box as="span" flex='1' textAlign='left' padding='16px 0'>
+                                            <div className="flex gap-4">
+                                                <h1 className="text-headline3 font-headline3 text-gray-700" >0{index + 1}</h1>
+                                                <h1 className="text-headline3 font-headline3 text-black">{lesson.name}</h1>
+                                            </div>
+                                        </Box>
+                                        <AccordionIcon />
+                                    </AccordionButton>
+                                    <AccordionPanel paddingTop='8px'>
+                                        <ul className="text-body2 font-body2 text-gray-700">
+                                            {lesson.sub_lessons.sort((a, b) => a.sub_lesson_id - b.sub_lesson_id).map((sub_lesson, index) => {
+                                                return (
+                                                    <li className="flex items-center px-2 py-3 gap-4 hover:cursor-pointer" key={index}
+                                                        onClick={() => { navigate(`/courses/1/learning/${sub_lesson.sub_lesson_id}`) }}
+                                                    >
+                                                        <img src="/image/icon/no-watch.png" alt="icon-status" className="w-[18px] h-[18px]" />
+                                                        <p>{sub_lesson.name}</p>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </AccordionPanel>
+                                </AccordionItem>
                             )
                         })}
                     </Accordion>
                 </div>
 
                 {/* ———————— Right Section ———————— */}
-                {isLoading? 
-                <div className="w-[68%] flex justify-center items-center">
-                <Spinner
-                thickness='5px'
-                speed='0.5s'
-                emptyColor='gray.200'
-                color='blue.500'
-                width={100}
-                height={100}
-                />
-                </div>
-                :
-                <div className="w-[68%] flex flex-col gap-8">
-                    {/* ———————— When Click Sub-Lesson This name and video will change (maybe create some state change when onclick) ———————— */}
-                    <div className="text-headline2 font-headline2 text-black">{lessonName}</div>
+                {isLoading ?
+                    <div className="w-[68%] flex justify-center items-center">
+                        <Spinner
+                            thickness='5px'
+                            speed='0.5s'
+                            emptyColor='gray.200'
+                            color='blue.500'
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    :
+                    <div className="w-[68%] flex flex-col gap-8">
+                        {/* ———————— When Click Sub-Lesson This name and video will change (maybe create some state change when onclick) ———————— */}
+                        <div className="text-headline2 font-headline2 text-black">{lesson?.name}</div>
                         {/* ———————— Video Section ———————— */}
-                    <video
-                            src={lessonVideo?.url}
+                        <video
+                            src={lesson?.video.url}
                             controls
                             className="rounded-lg w-[100%] mb-[50px]"
                             onPlay={handlePlayVideo}
@@ -160,31 +197,31 @@ function CourseLearningPage() {
                             onLoadedMetadata={handleLoadedMetadata}
                         />
 
-                    {/* ———————— Assignment Card ———————— */}
-                    <div className="bg-blue-100 w-full rounded-lg flex flex-col p-6 gap-[25px]">
+                        {/* ———————— Assignment Card ———————— */}
+                        <div className="bg-blue-100 w-full rounded-lg flex flex-col p-6 gap-[25px]">
 
-                        <div className="flex justify-between">
-                            <p className="text-body1 font-body1 text-black">Assignment</p>
-                            <Badge colorScheme='yellow' variant='solid' textTransform="capitalize">Pending</Badge>
-                        </div>
+                            <div className="flex justify-between">
+                                <p className="text-body1 font-body1 text-black">Assignment</p>
+                                <Badge colorScheme='yellow' variant='solid' textTransform="capitalize">Pending</Badge>
+                            </div>
 
-                        <div className="flex flex-col gap-1">
-                            <p>What are the 4 elements of service design?</p>
-                            <textarea name="answer-assignment" id="answer-assignment" cols="30" rows="10"
-                            placeholder="Answer..." className="w-full h-[100px] resize-none hide-scroll p-3 rounded-lg border border-gray-400 outline-none"></textarea>
-                        </div>
+                            <div className="flex flex-col gap-1">
+                                <p>What are the 4 elements of service design?</p>
+                                <textarea name="answer-assignment" id="answer-assignment" cols="30" rows="10"
+                                    placeholder="Answer..." className="w-full h-[100px] resize-none hide-scroll p-3 rounded-lg border border-gray-400 outline-none"></textarea>
+                            </div>
 
-                        <div className="flex justify-between items-center">
-                            <Button variant='primary' onClick={()=>{console.log(statusVideo)}}>Send Assignment</Button>
-                            <p className="text-gray-700">Assign within 2 days</p>
+                            <div className="flex justify-between items-center">
+                                <Button variant='primary' onClick={() => { console.log(statusVideo) }}>Send Assignment</Button>
+                                <p className="text-gray-700">Assign within 2 days</p>
+                            </div>
+
                         </div>
 
                     </div>
 
-                </div>
-                
                 }
-                
+
 
             </section>
 
