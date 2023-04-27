3e4589ed74b42e5db638ed881919276b10512d2a
suthee <lovalek@gmail.com>
2023-04-27 14:29:36 +0700

    feat(admin): add new edit lesson page

--- client/src/pages/admin/AddLessonPage.jsx
+++ client/src/pages/admin/AddLessonPage.jsx
@@ -6,6 +6,12 @@ import { Button, Input, Divider } from "@chakra-ui/react";
 const AddLessonPage = () => {
   const [value, setValue] = React.useState("");
   const handleChange = (event) => setValue(event.target.value);
+  const [videoFile, setVideoFile] = React.useState(null);
+
+  const handleVideoChange = (event) => {
+    const file = event.target.files[0];
+    setVideoFile(file);
+  };
   return (
     <section className="flex">
       {/* ————————————— Left Section ————————————— */}
@@ -15,7 +21,7 @@ const AddLessonPage = () => {
         {/* ————————————— Navbar Section ————————————— */}
         <nav
           className="h-[100px] border-gray-400 border-b bg-white flex justify-between 
-        px-10 py-4 items-center"
+        px-20 py-4 items-center"
         >
           <div className="flex items-center gap-4">
             <ArrowBackIcon boxSize={7} color="#9AA1B9" />
@@ -40,8 +46,8 @@ const AddLessonPage = () => {
         <div className="flex justify-center bg-[#F6F7FC]">
           {/* ————————————— Start Coding Here ————————————— */}
           <div
-            className="flex flex-col bg-white m-10 w-[1460px] h-[770px]
-        px-[5%] py-[2%] rounded-3xl border border-gray-400"
+            className="flex flex-col bg-white m-10 w-[1480px] h-[770px]
+            px-[5%] py-[2%] rounded-3xl border border-gray-400"
           >
             <h1 className="text-body2 text-black mb-1 mt-1">Lesson name *</h1>
             <Input
@@ -54,13 +60,14 @@ const AddLessonPage = () => {
               h="45px"
             />
             <Divider h="40px" mb="10px" />
-            {/* ————————————— Sub Lesson box ————————————— */}
+
             <h1 className="text-xl font-semibold text-[#646D89] py-5">
               Sub-Lesson
             </h1>
+            {/* ————————————— Sub Lesson box ————————————— */}
             <div
               className="flex flex-row justify-between h-[380px] bg-gray-100 border border-[#E4E6ED]
-          rounded-lg py-6 px-6"
+              rounded-lg py-6 px-6"
             >
               <img
                 src="/image/icon/drag.png"
@@ -82,14 +89,20 @@ const AddLessonPage = () => {
                   color="#D6D9E4"
                 />
                 <h1 className="text-[#07090D] text-body2 py-6">Video*</h1>
-                <div
-                  className="bg-gray-100 w-[200px] h-[200px] rounded-2xl 
-              flex flex-col justify-center
-              text-center text-blue-400 shadow-shadow2 hover:opacity-50"
-                >
-                  <h1 className="text-[40px]">+</h1>
-                  <h1 className="text-[20px]">Upload Video</h1>
-                </div>
+                
+                <label htmlFor="video-upload">
+                  <div className="bg-gray-100 w-[200px] h-[200px] rounded-2xl flex flex-col justify-center text-center text-blue-400 shadow-shadow2 hover:opacity-50">
+                    <input
+                      type="file"
+                      accept="video/*"
+                      onChange={handleVideoChange}
+                      id="video-upload"
+                      style={{ display: "none" }}
+                    />
+                    <h1 className="text-[40px]">+</h1>
+                    <h1 className="text-[20px]">Upload Video</h1>
+                  </div>
+                </label>
               </div>
               <div className="text-gray-500 font-bold text-base">Delete</div>
             </div>
